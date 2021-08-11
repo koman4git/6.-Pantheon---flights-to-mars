32289f3f3d2ef9ee29cab718b53bc8014f189f76
Danila Lazarew <koman4bk@bk.ru>
2021-08-11 22:22:42 +0300

    add gulpfile.js and plagins with him

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path={
+  build:{
+      html: project_folder+"/",
+      img: project_folder+ "/images/",
+      
+  },
+  src: {
+      html: source_folder+"/**/*.html", 
+      images: source_folder+ "/images/**/*.{jpg,png,svg,gif.ico,webp}",
+    },
+  watch: {
+    html: source_folder+"/**/*.html",
+    images: source_folder+ "/images/**/*.{jpg,png,svg,gif.ico,webp}",
+  },
+  clean: "./" + project_folder + "/"
+}
+
+let {src, dest} = require('gulp');
+    gulp = require('gulp'),
+    browsersync = require('browser-sync').create();
+    fileinclude = require('gulp-file-include');
+    del = require('del');
+    imagemin = require('gulp-imagemin');
+    cleanCSS = require('gulp-clean-css');
+    webp = require('gulp-webp');
+             
+
+function browserSync(params) {
+  browsersync.init({
+    server:{
+      baseDir: "./" + project_folder + "/"
+    },
+    port: 3000,
+    notify: false
+  })
+}
+
+function html(){
+  return src(path.src.html)
+      .pipe(fileinclude())
+      .pipe(dest(path.build.html))
+      .pipe(browsersync.stream())
+}
+function img(){
+  return src(path.src.images)
+      .pipe(dest(path.build.img))
+      .pipe(browsersync.stream())
+}
+
+
+function watchFiles(params){
+    gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.images], img);
+}
+
+function clean(params){
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(html, img));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.img = img;
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
